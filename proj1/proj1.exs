73b71010b5f2dd04d20dc4ff0b2fc460359d3afe
laocao95 <zhiwei.cao@ufl.edu>
2019-08-31 14:42:23 -0400

    [update]

--- proj1/proj1.exs
+++ proj1/proj1.exs
@@ -1,3 +1,19 @@
 # IO.inspect System.argv
-chunks = System.schedulers_online
-IO.puts(chunks)
+core_count = System.schedulers_online
+
+# IO.puts(core_count)
+
+startNum = String.to_integer(Enum.at(System.argv, 0))
+endNum = String.to_integer(Enum.at(System.argv, 1))
+
+chunk = div(endNum - startNum, core_count)
+# IO.puts(IEx.Helpers.i(startNum))
+# IO.puts(IEx.Helpers.i(endNum))
+for n <- 0..core_count-1, do:
+(
+    if n == core_count-1 do
+        VamNum.vampire_num({startNum + n * chunk, endNum})
+    else
+        VamNum.vampire_num({startNum + n * chunk, startNum + (n + 1) * chunk})
+    end
+)
