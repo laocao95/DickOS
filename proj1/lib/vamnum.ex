791dbcd3ecea8149ca6db22c3fdbd43cafc94d25
wytizxy <wytizxy@163.com>
2019-09-01 16:26:49 -0400

    delete 00

--- proj1/lib/vamnum.ex
+++ proj1/lib/vamnum.ex
@@ -5,7 +5,7 @@ defmodule VamNum do
             for n <- m..999, do:
             (
                 target = m * n
-                if rem(target - m - n, 9) == 0 && target >= low && target <= high do
+                if rem(target - m - n, 9) == 0 && target >= low && target <= high && rem(target, 100) != 0 do
                     start_digit = [div(m, 100), rem(div(m, 10), 10), rem(m, 10), div(n, 100), rem(div(n, 10), 10), rem(n, 10)]
                     product_digit = [div(target, 100000), rem(div(target, 10000), 10), rem(div(target, 1000), 10), rem(div(target, 100), 10), rem(div(target, 10), 10), rem(target, 10)]
 
