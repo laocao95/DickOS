d0fa6843190a4926ff41b00cf22ff58fbc0b1a91
wytizxy <wytizxy@163.com>
2019-09-28 14:00:25 -0400

    more topology

--- proj2/lib/topology.ex
+++ proj2/lib/topology.ex
@@ -49,12 +49,89 @@ defmodule Topology do
         end
     end
 
-    # def torus3D(numNodes) do
-    #     num = ceil(:math.pow(numNodes, 1 / 3))          # rounded cube side length
-    #     for x <- 1..num, y <- 1..num, z <- 1..num do
-            
-    #     end
-    # end
+    def torus3D(numNodes) do
+        num = ceil(:math.pow(numNodes, 1 / 3))          # rounded cube side length
+        for z <- 1..num, y <- 1..num,  x <- 1..num do
+            up = 
+            if z == num do
+                [getNum(x, y, 1, num)]
+            else
+                [getNum(x, y, z + 1, num)]
+            end
+            down = 
+            if z == 1 do
+                [getNum(x, y, num, num)]
+            else
+                [getNum(x, y, z - 1, num)]
+            end
+            left = 
+            if x == 1 do
+                [getNum(num, y, z, num)]
+            else
+                [getNum(x - 1, y, z, num)]
+            end
+            right = 
+            if x == num do
+                [getNum(1, y, z, num)]
+            else
+                [getNum(x + 1, y, z, num)]
+            end
+            front = 
+            if y == 1 do
+                [getNum(x, num, z, num)]
+            else
+                [getNum(x, y - 1, z, num)]
+            end
+            back = 
+            if y == num do
+                [getNum(x, 1, z, num)]
+            else
+                [getNum(x, y + 1, z, num)]
+            end
+            if num == 1 do
+                neighbors = []
+            else
+                neighbors = up ++ down ++ left ++ right ++ front ++ back
+            end
+        end
+    end
+
+    def honeyComb(numNodes) do
+        numCeil = trunc(6 * :math.pow(ceil(:math.pow(numNodes / 6, 1 / 2)), 2)) # rounded node number
+        neighborList = 
+        for num <- 1..numCeil do
+            level = floor(:math.pow(num / 6, 1 / 2))
+            levelIndex = num - 6 * level * level                 # the index of num in this level
+            levelEnd = 6 * (level + 1) * (level + 1)            # the index of the last element in the level
+            levelStart = 6 * level * level                      # the index of the last element in last level
+            numPerGroup = 2 * (level + 1) - 1
+            groupNum = ceil(levelIndex / numPerGroup)
+            groupIndex = levelIndex - numPerGroup * (groupNum - 1)
+
+            next =                                               # next one in same level
+            if num == levelEnd && do
+                [levelStart + 1]
+            else
+                [num + 1]
+            end
+
+            last = 
+            if num == levelStart + 1 do
+                [levelEnd]
+            else
+                [num - 1]
+            end
+
+            between = 
+            if rem(groupIndex, 2) != 0 do
+                [getHoney(level + 2, groupNum, groupIndex + 1)]
+            else
+                [getHoney(level, groupNum, groupIndex - 1)]
+            end
+
+            neighborList = [next] ++ [last] ++ [between]
+        end
+    end
 
     def getNeighbor(numNodes, topology) do
         cond do
@@ -64,8 +141,10 @@ defmodule Topology do
                 Topology.line(numNodes)
             topology == "rand2D" ->
                 Topology.rand2D(numNodes)
-            # topology == "3Dtorus" ->
-            #     Topology.torus3D(numNodes)
+            topology == "3Dtorus" ->
+                Topology.torus3D(numNodes)
+            topology == "honeycomb" ->
+                Topology.honeyComb(numNodes)
         end
     end
 
@@ -76,11 +155,14 @@ defmodule Topology do
         dis
     end
 
-    # def getNum(x, y, z, num) do
-    #     trunc(x + )
-    # end
+    def getNum(x, y, z, num) do
+        x + (y - 1) * num + (z - 1) * num * num
+    end
 
+    def getHoney(x, y, z) do
+        6 * (x - 1) * (x - 1) + (y - 1) * (2 * x - 1) + z
+    end
 end
 
 
-# IO.inspect(Topology.getNeighbor(20, "3Dtorus"))
+IO.inspect(Topology.getNeighbor(12, "honeycomb"))
